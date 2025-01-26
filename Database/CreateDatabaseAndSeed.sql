b3a7e7a5c1d408b41e548875e94d97ee995dc7ef
Tamesh Sivaguru <tam.s89@gmail.com>
2025-01-26 17:17:36 +0000

    Consolidated db files

--- Database/CreateDatabaseAndSeed.sql
+++ Database/CreateDatabaseAndSeed.sql
@@ -16,3 +16,17 @@ CREATE TABLE [dbo].[Podcasts]
     [TItle] NVARCHAR(MAX) NOT NULL,
 );
 GO
+
+-- Insert rows into table 'Podcasts' in schema '[dbo]'
+INSERT INTO [dbo].[Podcasts]
+( -- Columns to insert data into
+ [Title]
+)
+VALUES
+('Podcast 1'),
+('Podcast 2'),
+('Podcast 3'),
+('Podcast 4'),
+('Podcast 5')
+ 
+GO
--- Database/wait-and-run.sh
+++ Database/wait-and-run.sh
@@ -2,22 +2,22 @@
 
 # Function to check if the database is up and running
 is_database_up() {
-    nc -z database 5432
+    /opt/mssql-tools/bin/sqlcmd -S database -U sa -P Dometrain#1234 -Q "SELECT 1" > /dev/null 2>&1
     return $?
 }
 
 # Wait for the database to be up and running
 echo "Waiting for the database to be up and running..."
-until is_database_up; do
+until is_database_up -eq 0; do
     sleep 1
 done
 
 echo "Database is up and running."
 
 # Run the SQL scripts in the current directory
-for sql_file in *.sql; do
+for sql_file in /sql/*.sql; do
     echo "Running $sql_file..."
-    psql -h database -U sa -P Dometrain#1234 -d master -i "$sql_file"
+    /opt/mssql-tools/bin/sqlcmd -S database -U sa -P Dometrain#1234 -d master -i "$sql_file"
 done
 
 echo "All SQL scripts have been executed."
