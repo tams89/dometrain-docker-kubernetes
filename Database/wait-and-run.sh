67c80b893ba05fff9c28644633425d80cd5de070
Tamesh Sivaguru <tam.s89@gmail.com>
2025-01-26 17:30:28 +0000

    - use single sql script - updated password

--- Database/wait-and-run.sh
+++ Database/wait-and-run.sh
@@ -2,22 +2,23 @@
 
 # Function to check if the database is up and running
 is_database_up() {
-    /opt/mssql-tools/bin/sqlcmd -S database -U sa -P Dometrain#1234 -Q "SELECT 1" > /dev/null 2>&1
+    /opt/mssql-tools18/bin/sqlcmd -S database -U sa -P Dometrain#123 -Q "SELECT 1" > /dev/null 2>&1
     return $?
 }
 
 # Wait for the database to be up and running
-echo "Waiting for the database to be up and running..."
 until is_database_up -eq 0; do
-    sleep 1
+    /opt/mssql-tools18/bin/sqlcmd -C -S database -U sa -P Dometrain#123 -Q "SELECT 1" > /dev/null 2>&1
+    if [ $? -eq 0 ]
+    then 
+        echo "Database is up and running."
+        break
+    else
+        echo "Waiting for the database to be up and running..."
+        sleep 1
+    fi
 done
 
-echo "Database is up and running."
-
-# Run the SQL scripts in the current directory
-for sql_file in /sql/*.sql; do
-    echo "Running $sql_file..."
-    /opt/mssql-tools/bin/sqlcmd -S database -U sa -P Dometrain#1234 -d master -i "$sql_file"
-done
-
+# Run the SQL script
+/opt/mssql-tools18/bin/sqlcmd -C -S database -U sa -P Dometrain#123 -d master -i /CreateDatabaseAndSeed.sql
 echo "All SQL scripts have been executed."
