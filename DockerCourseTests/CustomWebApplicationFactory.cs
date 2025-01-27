d488c3eea4d920caf2abbeb98401872881004b1a
Tamesh Sivaguru <tam.s89@gmail.com>
2025-01-27 22:38:05 +0000

    created test proj with simple tests unit and integration

---
+++ DockerCourseTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,21 @@
+﻿using DockerCourseApi;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace DockerCourseTests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration(configBuilder =>
+            {
+                configBuilder.AddInMemoryCollection(
+                [
+                    new KeyValuePair<string, string?>("ConnectionString", "Server=tcp:localhost;Initial Catalog=podcasts;User Id=sa;Password=Dometrain#123;"),
+                ]);
+            });
+        }
+    }
+}
---
+++ DockerCourseTests/Tests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace DockerCourseTests
+{
+    [TestClass]
+    public sealed class Tests
+    {
+        [TestMethod]
+        public void Assert_True_Is_True()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void Assert_False_Is_False()
+        {
+            Assert.IsFalse(false);
+        }
+
+        [TestMethod]
+        public async Task GivenGetRequestToPodCastsEndpoint_ShouldReturnOkay()
+        {
+            var httpClient = new CustomWebApplicationFactory().CreateClient();
+            var response = await httpClient.GetAsync("/podcasts");
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
