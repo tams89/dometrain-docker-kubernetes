e6e0c0f969200d8754002a4ce16bf8e7d15ac206
Tamesh Sivaguru <tam.s89@gmail.com>
2025-01-27 22:39:25 +0000

    - Reference API project - Add test line in dockerfile for API

--- DockerCourseApi/Program.cs
+++ DockerCourseApi/Program.cs
@@ -1,18 +1,30 @@
 using Dapper;
+using Microsoft.Extensions.Options;
 using System.Data.SqlClient;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddCors();
-var app = builder.Build();
-app.UseCors(x => x.AllowAnyOrigin());
-
-app.MapGet("/podcasts", async () =>
+namespace DockerCourseApi
 {
-    var db = new SqlConnection("Server=database;Initial Catalog=podcasts;User Id=sa;Password=Dometrain#123;");
-    var podcasts = (await db.QueryAsync<Podcast>("SELECT * FROM Podcasts")).Select(x => x.Title);
-    return podcasts;
-});
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddCors();
+            builder.Services.Configure<Settings>(builder.Configuration);
 
-app.Run();
+            var app = builder.Build();
+            app.UseCors(x => x.AllowAnyOrigin());
 
-record Podcast(Guid Id, string Title);
+            app.MapGet("/podcasts", async (IOptions<Settings> settings) =>
+            {
+                var db = new SqlConnection(settings.Value.ConnectionString);
+                var podcasts = (await db.QueryAsync<Podcast>("SELECT * FROM Podcasts")).Select(x => x.Title);
+                return podcasts;
+            });
+
+            app.Run();
+        }
+    }
+
+    record Podcast(Guid Id, string Title);
+}
